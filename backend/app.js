c5de24960e26e3cf5d37a59851ac973668236b36
Muhammed Hesham <138380445+testomai@users.noreply.github.com>
2023-10-11 18:48:23 +0300

    initialize react project

--- backend/app.js
+++ backend/app.js
@@ -3,29 +3,29 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
-const connectToMongoDB = require("./DBConfig.js");
+const connectToMongoDB = require('./DBConfig.js');
 const port = process.env.PORT || 4000;
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const patientRouter = require('./Routes/patientRoutes.js');
 const doctorRouter = require('./Routes/doctorRoutes.js');
-const adminRoutes = require("./Routes/adminRoutes");
-const appointmentRouter = require("./Routes/appointmentRoutes.js");
-const packageRouter = require("./Routes/packageRoutes");
+const adminRoutes = require('./Routes/adminRoutes');
+const appointmentRouter = require('./Routes/appointmentRoutes.js');
+const packageRouter = require('./Routes/packageRoutes');
 
 app.use('/api/v1/patients', patientRouter);
 app.use('/api/v1/doctors', doctorRouter);
-app.use("/api/admins", adminRoutes);
-app.use("/api/v1/appointments", appointmentRouter);
-app.use("/api/v1/packages", packageRouter);
+app.use('/api/admins', adminRoutes);
+app.use('/api/v1/appointments', appointmentRouter);
+app.use('/api/v1/packages', packageRouter);
 
 connectToMongoDB();
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}`);
 });
