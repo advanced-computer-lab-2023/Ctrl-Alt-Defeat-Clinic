de5570455ac915398e13e9e33cc523c64cd8f56f
Muhammed Hesham <muhammedheshamm1@gmail.com>
2023-10-13 16:01:02 +0300

    update patient controller

--- backend/Routes/patientRoutes.js
+++ backend/Routes/patientRoutes.js
@@ -1,16 +1,17 @@
-const express = require("express");
-const patientController = require("../Controllers/patientController");
-const { protect, restrictTo } = require("../Middlewares/authMiddlewares");
+const express = require('express');
+const patientController = require('../Controllers/patientController');
+const { protect, restrictTo } = require('../Middlewares/authMiddlewares');
 
 const router = express.Router();
 
-
-router.route("/searchPatientsByName").get(patientController.searchPatientsByName);
-router.route("/register").post(patientController.registerPatient);
-router.route("/addMember/:id").post(patientController.addFamilyMember);
-router.route("/viewFamilyMembers").get(patientController.viewFamilyMembers);
-router.route("/viewPatients").get(patientController.viewAllPatients);
-
-router.route("/viewDoctors/:username").get(patientController.viewAllDoctors);
+router.route('/searchPatientsByName').get(patientController.searchPatientsByName);
+router.route('/register').post(patientController.registerPatient);
+router.route('/addMember/:id').post(patientController.addFamilyMember);
+router.route('/viewFamilyMembers').get(patientController.viewFamilyMembers);
+router.route('/viewPatients').get(patientController.viewAllPatients);
+router.route('/getAllPrescriptionsForPatient').get(patientController.getAllPrescriptionsForPatient);
+router.route('/getPrescriptionById').get(patientController.getPrescriptionById);
+router.route('/filterPrescriptions').get(patientController.filterPrescriptions);
+router.route('/viewDoctors/:username').get(patientController.viewAllDoctors);
 
 module.exports = router;
